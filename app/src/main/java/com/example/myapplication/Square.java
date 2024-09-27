b0339fc1f9a6c7d44ae15255ae9e4cfde50df9e1
Mohammedmostain <122962236+Mohammedmostain@users.noreply.github.com>
2024-09-26 19:17:39 -0600

    Ishfaq

--- app/src/main/java/com/example/myapplication/Square.java
+++ app/src/main/java/com/example/myapplication/Square.java
@@ -1,15 +1,2 @@
-package com.example.myapplication;
-
-public class Square extends Shape{
-    private int size_side;
-
-    // Constructor
-    public Square(int size_side) {
-        this.size_side = size_side;
-    }
-
-    public double area() {
-        return size_side * size_side; // Area of a square = side^2
-    }
-
+package com.example.myapplication;public class Square {
 }
